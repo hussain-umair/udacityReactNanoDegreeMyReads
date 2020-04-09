a942ca30f906abfe5a6a085e80490ec6b93de4a6
hussainumair <16b-024-bs@students.uit.edu>
2020-04-09 19:19:39 +0500

    [fix] book shelf checks every book object that has image thumbnail or not

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -15,7 +15,7 @@ return(
                             <li key={book.title}>
                                 <div className="book">
                                     <div className="book-top">
-                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${typeof book.imageLinks !== 'undefined'&&book.imageLinks.thumbnail})` }}></div>
                                         <div className="book-shelf-changer">
                                         <select defaultValue={book.shelf} name={book.title} onChange={(event)=>handleChange(event,book)}>
                                             <option value="move" disabled>Move to...</option>
